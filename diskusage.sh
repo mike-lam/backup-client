2c459f92c0a21e04c8279b9127b02aedd9da4c10
Ubuntu <vmadmin@ubuntu-gitlabstack05.j5bptqmcza4uvpu5i0ekuzskuh.ux.internal.cloudapp.net>
2018-11-21 20:19:10 +0000

    sample configs in docker-compose-ubunty.yml

--- diskusage.sh
+++ diskusage.sh
@@ -1,17 +1,30 @@
 #!/bin/bash
+#set -x #Display commands and their arguments as they are executed.
+#set -v #Display shell input lines as they are read.
 
 # these GLOBAL variables should be set in docker-compose.yml file as environment variables.
 
 #GLOBAL_VARIABLES
-USED_PERCENTAGE_THRESHOLD=75
-LOG_SIZE=10000
+#USED_PERCENTAGE_THRESHOLD when disk space % > threshold send email
+#FROM email of sender
+#TO email to receive notification when disk space % > threshold
+#SMTP_USER gmail user (without @gmail.com) 
+#SMTP_PASS   gmail password
+#LOG_SIZE  nbr of lines to keep in backup-client.log
 
 #these GLOBAL variables are calculated
+setNODE_HOSTNAME() {
+  #assumes the container has volume "/etc:/usr/local/data"
+  NODE_HOSTNAME=$(cat /usr/local/data/hostname 2> /dev/null)
+}
+setNODE_HOSTNAME
 
+#functions
 setUSED_PERCENTAGE_STR() {
-  usedStr=$(df --output=pcent . )
+#  usedStr=$(df --output=pcent . ) not supported by all versions of df :(
+  usedStr=$(df . | grep "\d%")
   usedArray=($usedStr)
-  USED_PERCENTAGE_STR=${usedArray[1]}
+  USED_PERCENTAGE_STR=${usedArray[4]}
 }
 
 setUSED_PERCENTAGE() {
@@ -31,14 +44,17 @@ touch /var/log/diskusage.log
 echo "Starting check disk space usage at $(date)"  2>&1 | tee -a /var/log/diskusage.log
 
 setUSED_PERCENTAGE
-echo $USED_PERCENTAGE
 if (( $USED_PERCENTAGE > $USED_PERCENTAGE_THRESHOLD )) ; then
-  echo "**************************"  2>&1 | tee -a  /var/log/diskusage.log
-  echo "*Low disk space reached!!*"  2>&1 | tee -a  /var/log/diskusage.log
-  echo "**************************"  2>&1 | tee -a  /var/log/diskusage.log
+  rm /var/log/email.log
+  echo "*********************************************"  2>&1 | tee -a  /var/log/diskusage.log /var/log/email.log
+  echo "Low disk space reached on $NODE_HOSTNAME !!!!"  2>&1 | tee -a  /var/log/diskusage.log /var/log/email.log
+  echo "*********************************************"  2>&1 | tee -a  /var/log/diskusage.log /var/log/email.log
+  df -h  .  2>&1 | tee -a  /var/log/email.log 
+  cat /var/log/email.log | sendEmail -f $FROM -t $TO -u Low disk space reached on $NODE_HOSTNAME  -s smtp.gmail.com:587 -o tls=auto -xu $SMTP_USER -xp $SMTP_PASS
 fi
-df .  2>&1 | tee -a  /var/log/diskusage.log
+df -h .  2>&1 | tee -a  /var/log/diskusage.log
 
 echo "Ending check disk space usage at $(date)"  2>&1 | tee -a  /var/log/diskusage.log
 
 keep_only_log_tail
+
